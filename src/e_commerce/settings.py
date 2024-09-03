1ba9ab724f5af9c56607109d96380c53daadd7de
Artur Filipe <arturfilipe@MacBook-Air-de-Artur.local>
2024-09-03 14:01:20 -0300

    insert static root in src/e_commerce/settings.py

--- src/e_commerce/settings.py
+++ src/e_commerce/settings.py
@@ -117,6 +117,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+     os.path.join(BASE_DIR, "static_local")
+]
+
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "static_root")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
