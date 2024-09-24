2f293bc0735d0798e1dae9e041f3554f2a05d897
Artur Filipe <arturfilipe@MacBook-Air-de-Artur.local>
2024-09-24 18:25:32 -0300

    cart with orders math

--- src/carts/models.py
+++ src/carts/models.py
@@ -1,14 +1,18 @@
+from decimal import Decimal
 from django.conf import settings
 from django.db import models
+
 from django.db.models.signals import pre_save, post_save, m2m_changed
+
 from products.models import Product
+
 
 User = settings.AUTH_USER_MODEL
 
 class CartManager(models.Manager):
     def new_or_get(self, request):
         cart_id = request.session.get("cart_id", None)
-        qs = self.get_queryset().filter(id = cart_id)
+        qs = self.get_queryset().filter(id=cart_id)
         if qs.count() == 1:
             new_obj = False
             cart_obj = qs.first()
@@ -16,23 +20,23 @@ class CartManager(models.Manager):
                 cart_obj.user = request.user
                 cart_obj.save()
         else:
-            cart_obj = Cart.objects.new(user = request.user)
+            cart_obj = Cart.objects.new(user=request.user)
             new_obj = True
             request.session['cart_id'] = cart_obj.id
         return cart_obj, new_obj
 
-    def new(self, user = None):
+    def new(self, user=None):
         user_obj = None
         if user is not None:
             if user.is_authenticated:
                 user_obj = user
-        return self.model.objects.create(user = user_obj)
+        return self.model.objects.create(user=user_obj)
 
 class Cart(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null = True, blank = True)
-    products = models.ManyToManyField(Product, blank = True)
-    total = models.DecimalField(default = 0.00, max_digits=100, decimal_places = 2)
+    products = models.ManyToManyField(Product,  blank = True)
     subtotal = models.DecimalField(default = 0.00, max_digits=100, decimal_places = 2)
+    total = models.DecimalField(default = 0.00, max_digits=100, decimal_places = 2)
     updated = models.DateTimeField(auto_now = True)
     timestamp = models.DateTimeField(auto_now_add = True)
 
@@ -42,10 +46,7 @@ class Cart(models.Model):
         return str(self.id)
 
 def m2m_changed_cart_receiver(sender, instance, action, *args, **kwargs):
-  #print(action)
   if action == 'post_add' or action == 'post_remove' or action == 'post_clear':
-    #print(instance.products.all())
-    #print(instance.total)
     products = instance.products.all() 
     total = 0 
     for product in products: 
@@ -53,18 +54,13 @@ def m2m_changed_cart_receiver(sender, instance, action, *args, **kwargs):
     if instance.subtotal != total:
       instance.subtotal = total
       instance.save()
-    #print(total) 
-    instance.subtotal = total
-    instance.save()
 
 m2m_changed.connect(m2m_changed_cart_receiver, sender = Cart.products.through)
 
 def pre_save_cart_receiver(sender, instance, *args, **kwargs):
-  instance.total = instance.subtotal + 10 # considere o 10 como uma taxa de entrega
-
-def pre_save_cart_receiver(sender, instance, *args, **kwargs):
     if instance.subtotal > 0:
-        instance.total = instance.subtotal  + 10 # considere o 10 como uma taxa de entrega
+        #instance.total = instance.subtotal  + 10 # considere o 10 como uma taxa de entrega
+        instance.total = Decimal(instance.subtotal) * Decimal(1.80) # 8% de taxa
     else:
         instance.total = 0.00
 
--- src/carts/urls.py
+++ src/carts/urls.py
@@ -3,11 +3,14 @@ from django.urls import path
 app_name = "carts"
 
 from .views import (
-                        cart_home, 
+                        cart_home,
+                        checkout_home, 
                         cart_update,
+                        
                     )
 
 urlpatterns = [
     path('', cart_home, name='home'),
+    path('checkout/', checkout_home, name='checkout'),
     path('update/', cart_update, name='update')
 ]
